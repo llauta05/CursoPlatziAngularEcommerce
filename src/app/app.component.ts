14b93e8ad790f27db39a721697b2500f761bf12a
Lautaro Landin <landin.lautaro@gmail.com>
2020-11-03 15:32:46 -0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'platzi-angular';
+  amigos = ['Lauta', 'Lean', 'Mati', 'Kily', 'Sabri'];
+
+  obj = {};
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/mather.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '2',
+      image: 'assets/images/memoria.jpg',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '3',
+      image: 'assets/images/micro.jpg',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '4',
+      image: 'assets/images/placaDeVideo.jpg',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+  ];
+
+  addItem() {
+    this.amigos.push('Un amigo');
+  }
+
+  deleteItem(index: number) {
+    this.amigos.splice(index, 1);
+  }
+  clickProduct(id: number) {
+    console.log('product');
+    console.log(id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product.component';
+import { CartComponent } from './cart/cart.component';
+import { ExponentialPipe } from './exponential.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CartComponent,
+    ExponentialPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
